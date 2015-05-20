69746674034fe0c99522fbdd58a211e42a9bbd20
Jonathan Peck <peckjonathan@gmail.com>
2015-05-20 02:41:53 +0200

    Updated quotes

--- peckbot/QuoteBehavior.py
+++ peckbot/QuoteBehavior.py
@@ -1,14 +1,16 @@
 import urllib2
+import HTMLParser
 
 class QuoteBehavior(object):
     def __init__(self):
         self.name = "Quotes"
+        self.parser = HTMLParser.HTMLParser()
 
     def execute(self, bot, match, channel):
         categories = 'joel_on_software+paul_graham+prog_style+codehappy'
         if match is None:
             match = categories
         req = urllib2.urlopen('http://www.iheartquotes.com/api/v1/random?source={0}'.format(match))
-        data = req.read()
+        data = self.parser.unescape(req.read())
 
         bot.send_msg(data, channel)
